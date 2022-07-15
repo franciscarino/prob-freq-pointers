5a14f2865a0c14da54ee0b3310f4a6b32e5cfb9b
Bryan Pitman <pitman.bryan@gmail.com>
2022-07-15 16:23:54 -0700

    complete twoArr object

--- two-array-object.js
+++ two-array-object.js
@@ -1,3 +1,30 @@
-// add whatever parameters you deem necessary & write docstring
-function twoArrayObject() {
+"use strict";
+
+
+/** takes in two arrays and return an object of key(keyArr) value(valueArr) pairs*/
+function twoArrayObject(keyArr, valueArr) {
+  //declare a new var and set the value to an empty object
+  let keyValueObj = {};
+  //set key Arr at index 0 with the value of valueArr at index 0
+  for (let i = 0; i < keyArr.length; i++) {
+    if (valueArr[i] === undefined) {
+      valueArr[i] = null;
+    }
+    keyValueObj[keyArr[i]] = valueArr[i];
+
+  }
+  //create a single for loop
+  //if the value of valueArr is undefined set the value to null.
+  //return new var
+
+  return keyValueObj;
+
 }
+
+
+
+// twoArrayObject(['x', 'y', 'z'], [1, 2]);
+//   // {'x': 1, 'y': 2, 'z': null}
+
+// twoArrayObject(['a', 'b', 'c'], [1, 2, 3, 4]);
+//   // {'a': 1, 'b': 2, 'c': 3}
